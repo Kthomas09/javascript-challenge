79d0e6a0dd65a9d5a1a86d2d2bab1b0f66a7ff73
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-10-06 17:31:17 -0400

    the search button functionality works for at least the date. Have to check for the other fields.

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,7 +2,7 @@
 var tableData = data;
 // targeting the table body
 var table = d3.select("#table");
-// creating functionality for the search/filter button
+// targing functionality for the search/filter button
 var searchButton = d3.select("#filter-btn");
 // creating funcitonality to search/filter cities
 var dateInput = d3.select("#datetime");
@@ -15,11 +15,23 @@ var shapeInput = d3.select("#shape");
 data.forEach (obj => {
     var row = table.append("tr");
     Object.entries(obj).forEach(([key,value])=>{
-        console.log(`${key} and ${value}`)
+        // console.log(`${key} and ${value}`)
         var tdata = table.append("td");
         tdata.text(value);
     })
 });
+
+// functionality for the search button
+searchButton.on("click",function() {
+    table.html("");
+    console.log("Loading Your Filter Results ...");
+    var inputField = d3.select("#datetime");
+    var inputElement = inputField.property("value");
+    console.log(inputElement);
+    var inputArray = data.filter(x => x.datetime === inputElement);
+    console.log(inputArray) 
+
+})
 
 
 
