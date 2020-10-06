b877a1bcad2a3da3fe2e82f3551319b90b026762
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-10-06 17:06:59 -0400

    set up the input variables and revised the HTML to reflect.

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
+// targeting the table body
+var table = d3.select("#table");
+// creating functionality for the search/filter button
+var searchButton = d3.select("#filter-btn");
+// creating funcitonality to search/filter cities
+var dateInput = d3.select("#datetime");
+var cityInput = d3.select("#city");
+var stateInput = d3.select("#state")
+var countryInput = d3.select("#country");
+var shapeInput = d3.select("#shape");
 
-// YOUR CODE HERE!
+
+
+
+
+
