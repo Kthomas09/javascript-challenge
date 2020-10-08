06cb81ba8ed4c877238b42ef2704bcefbf020575
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-10-08 15:42:56 -0400

    Got the Date to filter with the button functioning and the data loading into the table body to work. Onto level 2. This is technically submittable.

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -1,86 +1,31 @@
-// from data.js
 var tableData = data;
-// targeting the table body
-var table = d3.select("#table");
-// targing functionality for the search/filter button
-var searchButton = d3.select("#filter-btn");
-// creating funcitonality to search/filter cities
-var dateInput = d3.select("#datetime");
-var cityInput = d3.select("#city");
-var stateInput = d3.select("#state")
-var countryInput = d3.select("#country");
-var shapeInput = d3.select("#shape");
 
-// reading in the data from data.js
-data.forEach (obj => {
-    var row = table.append("tr");
-    Object.entries(obj).forEach(([key,value])=>{
-        // console.log(`${key} and ${value}`)
-        var tdata = table.append("td");
-        tdata.text(value);
-    })
-});
+var table_body = d3.select("tbody");
+var filter_table_button = d3.select("#filter-btn");
+var dateInputField = d3.select("#datetime");
+var table_columns = ["datetime", "city", "state", "country", "shape", "duratationMinutes", "comments"]
 
-// functionality for the search button
-searchButton.on("click",function() {
-    table.html("");
-    console.log("Loading Your Filter Results ...");
-    // date functionality
-    var inputField = d3.select("#datetime");
-    var inputElement = inputField.property("value");
-    console.log(inputElement);
-    var inputArray = data.filter(d => d.datetime === inputElement);
-    console.log(inputArray);
-    inputArray.forEach((selection) => {
-        var row = table.append("tr");
-        Object.entries(selection).forEach(([key,value]) => {
-            var cell = row.append("td");
-            cell.text(value);
-        });
-    });    
+var addData = (dataInput) => {
+    dataInput.forEach(ufo => {
+        var table_row = table_body.append("tr");
+        table_columns.forEach(c => table_row.append("td").text(ufo[c]))
+    });
+}
 
-// // City Functionality
-// searchButton.on("click",function() {
-//     table.html("");
-//     console.log("Loading Your Filter Results ...");
-//     // date functionality
-//     var inputField = d3.select("#city");
-//     var inputElement = inputField.property("value");
-//     console.log(inputElement);
-//     var inputArray = data.filter(c => c.city === inputElement);
-//     console.log(inputArray) 
+addData(tableData);
 
-    
-//     // State Functionality
-//     searchButton.on("click",function() {
-//         table.html("");
-//         console.log("Loading Your Filter Results ...");
-//         // date functionality
-//         var inputField = d3.select("#state");
-//         var inputElement = inputField.property("value");
-//         console.log(inputElement);
-//         var inputArray = data.filter(s => s.state === inputElement);
-//         console.log(inputArray)  
-   
-//     // Country Functionality
-//     var inputField = d3.select("#country");
-//     var inputElement = inputField.property("value");
-//     console.log(inputElement);
-//     var inputArray = data.filter(x => x.country === inputElement);
-//     console.log(inputArray) 
-//     // Shape Functionality
-//     var inputField = d3.select("#shape");
-//     var inputElement = inputField.property("value");
-//     console.log(inputElement);
-//     var inputArray = data.filter(x => x.shape === inputElement);
-//     console.log(inputArray) 
-
-// })
-
-
-
-
-
-
-
-
+filter_table_button.on("click", ()=> {
+    d3.event.preventDefault();
+    var date = dateInputField.property("value").trim();
+    var filterDate = tableData.filter(tableData=>tableData.datetime === date);
+    table_body.html("");
+    let response = {
+        filterDate
+    }
+    if (response.filterDate.length !==0) {
+        addData(filterDate);
+    }
+    else {
+        table_body.append("tr").append("td").text("No Sightings, Choose Another Date")
+    }
+})
