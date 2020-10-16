9a80ce096ca709522b5bb7305815f4b87cd2119f
Kent Thomas <kent.thomas@Kents-MacBook-Air.local>
2020-10-16 13:30:45 -0400

    Reading in the ufo data for level 2

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -3,6 +3,7 @@ var tableData = data;
 var table_body = d3.select("tbody");
 var filter_table_button = d3.select("#filter-btn");
 var dateInputField = d3.select("#datetime");
+var cityInputField = d3.select("#city");
 var table_columns = ["datetime", "city", "state", "country", "shape", "duratationMinutes", "comments"]
 
 var addData = (dataInput) => {
@@ -28,4 +29,8 @@ filter_table_button.on("click", ()=> {
     else {
         table_body.append("tr").append("td").text("No Sightings, Choose Another Date")
     }
+});
+
+tableData.forEach(function(ufo){
+    console.log(ufo);
 })
